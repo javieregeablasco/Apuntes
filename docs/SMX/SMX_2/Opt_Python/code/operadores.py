5a8fbd37cb98b88a43130f8ba5cb8ddbf1d1c3f5
javieregeablasco <j.egeablasco@edu.gva.es>
2025-09-26 17:53:57 +0200

    xxx

--- docs/SMX/SMX_2/Opt_Python/code/operadores.py
+++ docs/SMX/SMX_2/Opt_Python/code/operadores.py
@@ -27,3 +27,14 @@
 # a = 12 == 3
 
 # print(a and 12>3)
+
+
+# variable = 25
+# variable -= 5
+# # variable = variable - 5 
+
+# print("valor de ", variable)
+a=3 + 8/60 + 29/60**2 + 40/60**3
+
+
+print((3 + 8/60 + 29/60**2 + 40/60**3)==a)
