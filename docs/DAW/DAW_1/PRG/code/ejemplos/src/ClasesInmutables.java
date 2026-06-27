f34b8d55ba8aab30023377efd657afcd74f07a48
javieregeablasco <j.egeablasco@edu.gva.es>
2026-06-27 07:56:58 +0000

    Jun 27, 2026, 9:56 AM

--- docs/DAW/DAW_1/PRG/code/ejemplos/src/ClasesInmutables.java
+++ docs/DAW/DAW_1/PRG/code/ejemplos/src/ClasesInmutables.java
@@ -3,7 +3,7 @@ public class ClasesInmutables {
     String palabra_1 = "palabra";
     String palabra_2 = "palabra";
 
-    if (palabra_1 == palabra_2){
+    if (palabra_1.equals(palabra_2)){
       System.out.println("las 2 palabras son la misma instancia");
     } else {
       System.out.println("Son 2 instancias diferentes");
@@ -13,7 +13,7 @@ public class ClasesInmutables {
     String palabra_3 = new String("palabra");
     String palabra_4 = new String("palabra");
 
-    if (palabra_3 == palabra_4){
+    if (palabra_3.equals(palabra_4)){
       System.out.println("las 2 palabras son la misma instancia");
     } else {
       System.out.println("Son 2 instancias diferentes");
