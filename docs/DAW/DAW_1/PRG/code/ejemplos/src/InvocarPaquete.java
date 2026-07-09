c11ea389ee7294be70bbfa074dad0a92c3c00d4a
javieregeablasco <j.egeablasco@edu.gva.es>
2026-07-09 09:17:13 +0000

    Jul 9, 2026, 11:17 AM

--- docs/DAW/DAW_1/PRG/code/ejemplos/src/InvocarPaquete.java
+++ docs/DAW/DAW_1/PRG/code/ejemplos/src/InvocarPaquete.java
@@ -1,7 +1,7 @@
-import paquete_1.Paquete1.*;
+// import paquete_1.Paquete1;
 
 public class InvocarPaquete {
   public static void main(String[] args) {
-    Paquete1.metodoPaquete1();
+    paquete_1.Paquete1.metodoPaquete1();
   }
 }
